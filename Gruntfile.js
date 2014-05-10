7c46d3139ac352a855ed703c1d4d0679b8bf6dea
Santhosh Babu <html5webster@gmail.com>
2014-05-10 02:55:13 -0700

    updated templates repo to confirm with server structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,39 @@ module.exports = function( grunt ) {
 	// configure task(s)
 	grunt.initConfig( {
 
+		connect: {
+		// connect begin
+			templateServer: {
+			// templateServer begin
+				options: {
+					  hostname: "localhost"
+					, port: 44210
+					, keepalive: true
+					, middleware:	function (connect, options) {
+										return [
+											  function (req, res, next) {
+												res.setHeader('Access-Control-Allow-Origin', '*');
+												res.setHeader('Access-Control-Allow-Methods', '*');
+												next();
+											  }
+											// Serve static files.
+											, connect.static(options.base)
+											// Make empty directories browsable.
+											, connect.directory(options.base)
+										];
+							    	}
+				}
+			// templateServer end
+			}
+		// connect end
+		}	
+
 	} );
 
 	// load task(s)
-	grunt.loadNpmTasks( "" );
+	grunt.loadNpmTasks( "grunt-contrib-connect" );
 
 	// register task(s)
-	grunt.registerTask( "", "" );
+	grunt.registerTask( "templateServer", "connect:templateServer" );
 
 };
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 Front-End Specific Template(s)
 
 **Contains**
-* HTML5 base template
-* HTML grid template
-* Gruntfile.js template
+* HTML5 template
+* Script template
+
+**launch Template Server**  ( [http://localhost:44210/](http://localhost:44210/) )
+```bash
+grunt templateServer
+```
